cdf431bb8c097d51a10c8b3fa81b2239656103f0
Heamex <1909.belov@gmail.com>
2023-03-22 21:09:56 +0300

    Initial Commit

---
+++ unsplashViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  unsplashViewer
+//
+//  Created by Nikita Belov on 22.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

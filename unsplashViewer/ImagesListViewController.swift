da0172ef58fb0e6396aeacba1b8c0696f09d07d3
Nikita Belov <1909.belov@gmail.com>
2023-04-09 22:04:59 +0300

    table linked with ImagesListViewController

--- unsplashViewer/ImagesListViewController.swift
+++ unsplashViewer/ImagesListViewController.swift
@@ -9,11 +9,11 @@ import UIKit
 
 class ImagesListViewController: UIViewController {
 
+	@IBOutlet var tableView: UITableView!
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
 
 }
 

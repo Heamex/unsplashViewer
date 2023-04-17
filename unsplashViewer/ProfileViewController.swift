7113a0c3c1dd625c0a086c9f88a69a7b02564be2
Nikita Belov <1909.belov@gmail.com>
2023-04-17 19:24:13 +0300

    Добавлен TabBar и NavigationController

---
+++ unsplashViewer/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  unsplashViewer
+//
+//  Created by Nikita Belov on 16.04.23.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+	
+	override var preferredStatusBarStyle: UIStatusBarStyle {
+		return .lightContent
+	}
+	
+	
+}

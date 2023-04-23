5fd22461167e6a8a35c5543e41f7264b82206c2c
Nikita Belov <1909.belov@gmail.com>
2023-04-23 21:52:25 +0300

    Добавлены данные с unsplash API

---
+++ unsplashViewer/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  unsplashViewer
+//
+//  Created by Nikita Belov on 23.04.23.
+//
+
+import Foundation
+
+let AccessKey = "i5Gsf8HqpDDb9LXtilndhUYty_ky-jkrZ2jwRN8sUaI"
+let SecretKey = "lpkcgx2QLTXw0VcbVLw1LdiUZATROgfKcF2spCctdq4"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")
